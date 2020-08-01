a720d91ded2b80e709d576e708c274407286eda1
justsomeguy <thenaders3@gmail.com>
2020-08-01 09:56:44 -0700

    uniswap v1 testing

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const express = require('express');
 const cors = require('cors');
 const Web3 = require('web3');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+const moment = require('moment');
+
+// ABIs
 const ABIS = require('./abis');
 
 // .env 
 const {
   SERVER_PORT,
   RPC_URL,
-  PRIV_KEY
+  PRIV_KEY,
+  ADDRESS
 } = process.env;
 
 // ABIs
@@ -33,13 +37,33 @@ const DAI_ADDRESS = '0xad6d458402f60fd3bd25163575031acdce07538d';
 const UNISWAP_ADDRESS = '0xc0fc958f7108be4060F33a699a92d3ea49b0B5f0';
 
 const daiContract = new web3.eth.Contract(DAI_ABI, DAI_ADDRESS);
-const uniswapContract = new web3.eth.Contract(UNISWAP_ABI, UNISWAP_ADDRESS);
+const uniswapV1DaiContract = new web3.eth.Contract(UNISWAP_ABI, UNISWAP_ADDRESS);
 
-const getBalance = async () => {
-  const daiDecimals = await daiContract.methods.decimals().call();
-  console.log(daiDecimals);
+const tradeEthForDai = async () => {
+  const ethAmount = web3.utils.toWei('0.001', 'Ether');
+  const daiAmount = await uniswapV1DaiContract.methods.getEthToTokenInputPrice(ethAmount).call()
+  console.log('Trade found, swapping Eth for Dai...');
+  const now = moment().unix();
+  const DEADLINE = now + 600;
+  // console.log("Deadline", DEADLINE);
+
+  const SETTINGS = {
+    gasLimit: 900000,
+    gasPrice: web3.utils.toWei('50', 'Gwei'),
+    from: ADDRESS,
+    value: ethAmount
+  }
+
+  try{
+    console.log('Trade submitted to the blockchain');
+    let result = await uniswapV1DaiContract.methods.ethToTokenSwapInput(daiAmount.toString(), DEADLINE).send(SETTINGS);
+    console.log(`Transaction: https://ropsten.etherscan.io/tx/${result.transactionHash}`);
+  }
+  catch(error) {
+    console.log('There was an error swapping: ', error);
+  }
 }
 
-getBalance();
+app.listen(SERVER_PORT, () => console.log(`Bot is starting up on Port ${SERVER_PORT}`));
 
-app.listen(SERVER_PORT, () => console.log(`Listening on Port ${SERVER_PORT}`));
+tradeEthForDai();
